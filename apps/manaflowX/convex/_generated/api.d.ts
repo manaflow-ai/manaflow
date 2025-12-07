966d630f463ef0f08665c596161a8f98eebe2c32
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-12-06 18:36:19 -0800

    wip

--- apps/manaflowX/convex/_generated/api.d.ts
+++ apps/manaflowX/convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as actions from "../actions.js";
 import type * as myFunctions from "../myFunctions.js";
 
 import type {
@@ -17,6 +18,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  actions: typeof actions;
   myFunctions: typeof myFunctions;
 }>;
 
