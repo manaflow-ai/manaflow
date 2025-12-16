56666fd478e90c0252072b5a2784f9fc8ad98cc5
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-12-16 00:30:57 -0800

    Use original morph URLs for vscode iframes instead of proxy

--- packages/www-openapi-client/src/client/types.gen.ts
+++ packages/www-openapi-client/src/client/types.gen.ts
@@ -420,7 +420,7 @@ export type SetupInstanceBody = {
     instanceId?: string;
     selectedRepos?: Array<string>;
     ttlSeconds?: number;
-    snapshotId?: string | ('snapshot_bv46m6us' | 'snapshot_xzvnwwc1' | 'snapshot_pcmfvjra');
+    snapshotId?: string | ('snapshot_n53wv98d' | 'snapshot_wgp0rrue' | 'snapshot_pcmfvjra');
 };
 
 export type InstanceInfo = {
