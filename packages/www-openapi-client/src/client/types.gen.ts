a08d9520a874f851dd0a871c471e7567a7427c61
austinpower1258 <austinwang115@gmail.com>
2025-12-19 17:18:44 -0800

    new morph snapshots

--- packages/www-openapi-client/src/client/types.gen.ts
+++ packages/www-openapi-client/src/client/types.gen.ts
@@ -420,7 +420,7 @@ export type SetupInstanceBody = {
     instanceId?: string;
     selectedRepos?: Array<string>;
     ttlSeconds?: number;
-    snapshotId?: string | ('snapshot_adpauztr' | 'snapshot_r0bk3bi9' | 'snapshot_pcmfvjra');
+    snapshotId?: string | ('snapshot_hoax9chh' | 'snapshot_jvzvjy40' | 'snapshot_pcmfvjra');
 };
 
 export type InstanceInfo = {
